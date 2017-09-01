8719ec5feebda19687480d3aa189e572389fbc57
minseong <damul0202@naver.com>
2017-09-01 11:58:07 +0900

    googleMap
    googleMap

--- KNU_Hackathon/app/src/main/java/dgsw/hs/kr/knu_hackathon/MainActivity.java
+++ KNU_Hackathon/app/src/main/java/dgsw/hs/kr/knu_hackathon/MainActivity.java
@@ -1,7 +1,10 @@
 package dgsw.hs.kr.knu_hackathon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView v1 = (TextView)findViewById(R.id.click);
+        v1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent( getApplicationContext() , MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
